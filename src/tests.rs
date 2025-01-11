204861f43c65930b22426fedc20e4a457a0475a9
Will Bickford <wbic16@gmail.com>
2025-01-10 21:38:27 -0600

    Fix tests

--- src/tests.rs
+++ src/tests.rs
@@ -9,7 +9,7 @@ fn test_insert() {
   let coordinate = phext::to_coordinate("1.1.1/1.1.1/1.1.2");
   let update = "Hello World!".to_string();
   let filename = "insert.phext".to_string();
-  let done = crate::sq::process(&mut scroll, command, &mut buffer, coordinate, update, filename);
+  let done = crate::sq::process(1, "memory".to_string(), &mut scroll, command, &mut buffer, coordinate, update, filename);
 
   assert_eq!(buffer, "\x17Hello World!");
   assert_eq!(done, false);
@@ -23,7 +23,7 @@ fn test_select() {
   let coordinate = phext::to_coordinate("1.1.1/1.1.1/1.1.3");
   let update = "ignored text".to_string();
   let filename = "select.phext".to_string();
-  let done = crate::sq::process(&mut scroll, command, &mut buffer, coordinate, update, filename);
+  let done = crate::sq::process(1, "memory".to_string(), &mut scroll, command, &mut buffer, coordinate, update, filename);
 
   assert_eq!(buffer, "\x17\x17Third Scroll Content");
   assert_eq!(scroll, "Third Scroll Content");
@@ -38,7 +38,7 @@ fn test_update() {
   let coordinate = phext::to_coordinate("1.1.1/1.1.1/1.2.2");
   let update = "Full Rewrite at 1.2.2".to_string();
   let filename = "update.phext".to_string();
-  let done = crate::sq::process(&mut scroll, command, &mut buffer, coordinate, update, filename);
+  let done = crate::sq::process(1, "memory".to_string(), &mut scroll, command, &mut buffer, coordinate, update, filename);
 
   assert_eq!(buffer, "\x17\x18\x17Full Rewrite at 1.2.2");
   assert_eq!(scroll, "Updated 21 bytes");
@@ -53,7 +53,7 @@ fn test_delete() {
   let coordinate = phext::to_coordinate("1.1.1/1.1.1/1.2.2");
   let update = "".to_string();
   let filename = "delete.phext".to_string();
-  let done = crate::sq::process(&mut scroll, command, &mut buffer, coordinate, update, filename);
+  let done = crate::sq::process(1, "memory".to_string(), &mut scroll, command, &mut buffer, coordinate, update, filename);
 
   assert_eq!(buffer, "\x17\x18\x17");
   assert_eq!(scroll, "Removed 21 bytes");
@@ -68,7 +68,7 @@ fn test_save() {
   let coordinate = phext::to_coordinate("1.1.1/1.1.1/1.2.2");
   let update = "Save Test at 1.2.2".to_string();
   let filename = "save.phext".to_string();
-  let done = crate::sq::process(&mut scroll, command, &mut buffer, coordinate, update, filename);
+  let done = crate::sq::process(1, "memory".to_string(), &mut scroll, command, &mut buffer, coordinate, update, filename);
 
   assert_eq!(buffer, "\x17\x18\x17Save Test");
   assert_eq!(scroll, "Wrote 12 bytes to save.phext");
@@ -85,7 +85,7 @@ fn test_exit() {
   let coordinate = phext::to_coordinate("1.1.1/1.1.1/1.1.1");
   let update = "Shutdown Test".to_string();
   let filename = "shutdown.phext".to_string();
-  let done = crate::sq::process(&mut scroll, command, &mut buffer, coordinate, update, filename);
+  let done = crate::sq::process(1, "memory".to_string(), &mut scroll, command, &mut buffer, coordinate, update, filename);
 
   assert_eq!(done, true);
 }
