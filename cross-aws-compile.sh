1d026a6a2e4750052aba63cda9cbc58cfed0ec87
Will Bickford <wbic16@gmail.com>
2025-07-01 01:45:29 -0500

    Use musl not glibc

--- cross-aws-compile.sh
+++ cross-aws-compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export RUSTFLAGS="-C linker=aarch64-linux-gnu-gcc"
-cargo build --release --target=aarch64-unknown-linux-gnu
-ls -l target/aarch64-unknown-linux-gnu/release/
+cargo build --release --target=aarch64-unknown-linux-musl
+scp target/aarch64-unknown-linux-musl/release/sq ec2-user@phext.io:/exo/sq
